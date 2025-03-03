bf08ec5a7d397da8647baa6a05db73ea7225e582
Yasya4444 <lukonina04an@mail.ru>
2025-03-03 16:27:24 +0400

    Update drum_library.sql

--- drum_library.sql
+++ drum_library.sql
@@ -1,20 +1,7 @@
-    USE drum_library;
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Advanced Techniques for the Modern Drummer', 'Jim Chapin', 'book1.jpg', 'book1.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Stick Control: For the Snare Drummer', 'George L. Stone', 'book2.jpg', 'book2.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('The New Breed', 'Gary Chester', 'book3.jpg', 'book3.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Modern Drummer', 'MD Staff', 'book4.jpg', 'book4.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Progressive Steps to Syncopation For The Modern Drummer', 'Ted Reed', 'book5.jpg', 'book5.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ("Alfred's Drum Method, Bk 1", 'Sandy Feldstein and Dave Black', 'book6.jpg', 'book6.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Realistic Rock Drum Method', 'Carmine Appice', 'book7.jpg', 'book7.pdf');
-    INSERT INTO books (title, author, image, pdf) VALUES
-    ('Drumset 101', 'Dan Britt', 'book8.jpg', 'book8.pdf');
-
-    SELECT * FROM books;
-    
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    image VARCHAR(255), -- Имя файла или путь к обложке
+    pdf VARCHAR(255)    -- Имя файла или путь к PDF
+);
