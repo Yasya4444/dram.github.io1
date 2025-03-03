2065cc0843402edfaa79a448d221dcd49454573f
Yasya4444 <lukonina04an@mail.ru>
2025-03-03 17:09:26 +0400

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,20 +1,6 @@
-<?php
-require_once 'db_config.php';
-
-$dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-try {
-     $pdo = new PDO($dsn, DB_USER, DB_PASSWORD, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
-// ... ваш код для работы с базой данных ...
-
-$pdo = null; // Закрываем соединение
-?>
-
+  <?php
+    define('DB_HOST', 'localhost'); // Или IP адрес вашего сервера MySQL
+    define('DB_USER', 'root');    // Имя пользователя базы данных
+    define('DB_PASSWORD', 'your_password'); // Пароль базы данных
+    define('DB_NAME', 'drum_library');   // Имя базы данных
+    ?>
