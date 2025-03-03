d4bc6753fee54719cd6e9ab727e416aa0c7fa4ca
Yasya4444 <lukonina04an@mail.ru>
2025-03-03 11:38:35 +0400

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,5 +1,5 @@
     <?php
-    define('DB_HOST', 'localhost');    // Или IP адрес вашего сервера MySQL
+    define('DB_HOST', '127.0.0.1');    // Или IP адрес вашего сервера MySQL
     define('DB_USER', 'root');   // Замените на имя пользователя базы данных
     define('DB_PASSWORD', 'neko2chan9O!'); // Замените на пароль базы данных
     define('DB_NAME', 'drum_library');  // Замените на имя базы данных
